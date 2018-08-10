049a57094ea9cd1bc92c731d36be91ebbec93da6
Justin V <justinvuser@gmail.com>
2018-08-10 13:45:16 -0400

    Validation of user input working in Survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,22 @@
 var path = require("path");
 var bodyParser = require("body-parser");
-var friendsData = require('../data/friends');
+var friends = require('../data/friends');
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friendsData);
       });
+
+      app.post('/api/friends', function(req, res) {
+        var userData = req.body;
+        var userScores = userData.scores;
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length; i++){
+            
+        }
+        });
+    
 
 };
 
