565c7a9696187ba557a4a6dcee3fc040850aa860
Justin V <justinvuser@gmail.com>
2018-08-10 12:19:28 -0400

    Survey.html changes

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+{
+    name:"Option 1",
+    photo:"",
+    scores:[
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      ]
+  },
+  {
+    name:"Option 2",
+    photo:"",
+    scores:[
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      ]
+  }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
    console.log("Connection made on PORT " + PORT);
